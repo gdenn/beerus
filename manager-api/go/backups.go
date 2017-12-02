e2b6ffc97a66b2d5b625a45b6f4b6bf228242ecc
gdenn <gdennis91@googlemail.com>
2017-12-02 15:26:51 +0000

    Generated code by SwaggerHub

---
+++ manager-api/go/backups.go
@@ -0,0 +1,50 @@
+package 
+
+import (
+	"net/http"
+)
+
+type Backups struct {
+
+}
+
+func CreateAgentBackup(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func CreateBackup(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func CreateBrokerBackup(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func DeleteBackup(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func ListAgentBackups(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func ListBackups(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func ListBrokerBackups(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func ListSpecificAgentBackups(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
---
+++ manager-api/go/brokers.go
@@ -0,0 +1,30 @@
+package 
+
+import (
+	"net/http"
+)
+
+type Brokers struct {
+
+}
+
+func BrokerDetails(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func CreateBroker(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func ListBrokers(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func RemoveBroker(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
